26ebb66bf5bae3fe9147eb0911a607a59a2539a0
Allan Arnt <arnthere@gmail.com>
2023-04-25 18:56:29 +0800

    initialize project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row">
+    <h1 class="h3 mb-3">About Page</h1>
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title mb-0">Empty card</h5>
+        </div>
+        <div class="card-body"></div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <h1 class="h3 mb-3">Blank Page</h1>
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title mb-0">Empty card</h5>
+        </div>
+        <div class="card-body"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HomeView",
+};
+</script>
